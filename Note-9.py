07a6215686143035fbee1fd0a0240eb5a1abd96c
vasanthi163 <vasanthi163>
2024-06-26 10:57:42 +0000

    commit

--- Note-9.py
+++ Note-9.py
@@ -24,4 +24,36 @@ display(df)
 
 # COMMAND ----------
 
+df.withColumn("topping",explode("topping")).display()
+
+# COMMAND ----------
+
+df1= df.withColumn("topping_id", col("topping.id")).withColumn("topping_type",col("topping.type"))
+display(df1)
+
+# COMMAND ----------
+
+### it merge the batters and batter
+df2=df1.withColumn("batters",explode("batters.batter"))
+display(df2)
+
+
+# COMMAND ----------
+
+df3 = df2.withColumn("batters_id", col("batters.id"))\
+.withColumn("batters_type",col("batters.type"))
+display(df3)
+
+# COMMAND ----------
+
+df_final=df3.write.mode("overwrite").parquet("dbfs:/mnt/demo-datasets/output")
+
+
+# COMMAND ----------
+
+df_final=spark.read.parquet("dbfs:/mnt/demo-datasets/output")
+display(df_final)
+
+# COMMAND ----------
+
 
