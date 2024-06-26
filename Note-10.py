ce317520c681a86e043ac294efc042f9b13b3ca8
vasanthi163 <vasanthi163>
2024-06-26 11:47:40 +0000

    commit

--- Note-10.py
+++ Note-10.py
@@ -1,2 +1,50 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ### End to End stream
+
+# COMMAND ----------
+
+df=spark.read.option("header",True).csv("dbfs:/FileStore/tables/Streamdata/input/march.csv")
+display(df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Creating user define schema
+
+# COMMAND ----------
+
+from pyspark.sql.types import *
+
+
+# COMMAND ----------
+
+users=StructType([StructField("Name",StringType()),StructField("Age",StringType()),StructField("City",StringType())])
+
+
+# COMMAND ----------
+
+df1=spark.readStream.schema(users).option("header",True).csv("dbfs:/FileStore/tables/Streamdata/input")
+display(df1)
+
+# COMMAND ----------
+
+df1.writeStream.option("format","parquet").outputMode("append").option("checkpointLocation","/FileStore/tables/Streamdata/checkpoint").option("path","/FileStore/tables/Streamdata/output").start()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Verfying o/p stream
+
+# COMMAND ----------
+
+from pyspark.sql.functions import *
+
+# COMMAND ----------
+
+dfnew=spark.read.format("delta").load("/FileStore/tables/Streamdata/output")
+display(dfnew)
+
+# COMMAND ----------
+
 
