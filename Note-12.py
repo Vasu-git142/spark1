2141c1d845c1336b7670789a29ddda72fe63c228
vasanthi163 <vasanthi163>
2024-07-01 10:45:08 +0000

    commit

--- Note-12.py
+++ Note-12.py
@@ -1,2 +1,29 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ##Internal delta lake
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC create database lake
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC use lake
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC create or replace table  people_1(
+# MAGIC   id INT,
+# MAGIC   firstname STRING,
+# MAGIC   lastname STRING,
+# MAGIC   gender STRING,
+# MAGIC   age INT,
+# MAGIC   salary INT
+# MAGIC ) location "dbfs:/user/hive/warehouse/external"
+
+# COMMAND ----------
+
 
