ad91f966a163be77e424fe0d7e488d0d49849a39
vasanthi163 <vasanthi163>
2024-07-01 07:31:29 +0000

    commit

--- Note-11.py
+++ Note-11.py
@@ -1,2 +1,34 @@
 # Databricks notebook source
+# MAGIC %sql
+# MAGIC create database deltatable
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC use deltatable
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC create table people3(
+# MAGIC   id INT,
+# MAGIC   firstname STRING,
+# MAGIC   lastname STRING,
+# MAGIC   gender STRING,
+# MAGIC   age INT,
+# MAGIC   salary INT
+# MAGIC ) 
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select *from people3
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC describe extended people3
+
+# COMMAND ----------
+
 
