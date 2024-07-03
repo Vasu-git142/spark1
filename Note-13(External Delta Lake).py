d543233d55f669d5fcac9e7a7bfc63c7c2aa071c
vasanthi163 <vasanthi163>
2024-07-03 07:06:52 +0000

    commit

--- Note-13(External Delta Lake).py
+++ Note-13(External Delta Lake).py
@@ -3,11 +3,20 @@ from pyspark.sql.functions import *
 
 # COMMAND ----------
 
-stu=spark.read.option("header",True).option("inferSchema",True).csv("/FileStore/tables/csv/school.csv")
+# MAGIC %sql
+# MAGIC create table d_b.stu(
+# MAGIC   id INT,
+# MAGIC   name STRING,
+# MAGIC   age INT,
+# MAGIC   gender STRING,
+# MAGIC   course STRING,
+# MAGIC   grade INT,
+# MAGIC   ingestiondate TIMESTAMP
+# MAGIC )
 
 # COMMAND ----------
 
-display(stu)
+d_b.stu.write.saveAsTable("stu")
 
 # COMMAND ----------
 
@@ -16,6 +25,15 @@ stu1=stu.withColumn("ingestion_date",current_timestamp())
 # COMMAND ----------
 
 display(stu1)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC create database d_b
+
+# COMMAND ----------
+
+stu1.write.saveAsTable("db.student_managed")
 
 # COMMAND ----------
 
