dbb644b1072a6e3abc52b6e9e0e906795fc01753
vasanthi163 <vasanthi163>
2024-07-03 09:34:49 +0000

    commit

--- Note-14(Views).py
+++ Note-14(Views).py
@@ -1,2 +1,31 @@
 # Databricks notebook source
+# MAGIC %md
+# MAGIC ##views
+# MAGIC 1)standard view
+# MAGIC 2)temp view
+# MAGIC 3)Global view
+
+# COMMAND ----------
+
+# MAGIC %fs ls dbfs:/databricks-datasets/bikeSharing/data-001/hour.csv
+
+# COMMAND ----------
+
+df=spark.read.option("header",True).option("inferSchema",True).csv("dbfs:/databricks-datasets/bikeSharing/data-001/hour.csv")
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+df.write.saveAsTable("bikes")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select * from bikes
+
+# COMMAND ----------
+
 
